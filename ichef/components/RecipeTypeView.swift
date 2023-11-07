042ae243a9c3184958bf1de001843d223e1c51bf
Laura Moroni <moronilaura28@gmail.com>
2023-11-06 21:07:05 -0300

    Coracao clicavel dos favoritos

--- ichef/components/RecipeTypeView.swift
+++ ichef/components/RecipeTypeView.swift
@@ -10,33 +10,55 @@ import SwiftUI
 struct RecipeTypeView: View {
     @State public var title : String
     @State public var image : String
+    @State private var isFavorited = false
     static var recipeTypes : [ SearchRecipe ] = sampleData
     var body: some View {
-        ZStack{
-            RoundedRectangle(cornerRadius: 12)
-                .frame(height: 75)
-                .foregroundColor(.black.opacity(0.39))
+        NavigationStack {
+            ZStack{
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(height: 75)
+                    .foregroundColor(.black.opacity(0.39))
                 
                 
-            HStack {
-                Spacer()
-                
-                Text(title)
+                HStack {
+                    AsyncImage(url: URL(string: image))
+                        .scaledToFill()
+                    
+                        .frame(width: 55, height: 55)
+                        .clipped()
+                        .cornerRadius(.infinity)
+                        .padding()
+                    
+                    Spacer()
+                    NavigationLink(title) {
+                       
+                            
+                    }
                     .font(.title)
                     .bold()
-                
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                
-                Image(systemName: "heart.fill")
-                    .resizable()
-                    .foregroundStyle(.gray, .red)
-                    .frame(width: 35, height: 30, alignment: .center)
-                
-                Spacer()
+                    .foregroundColor(.white)
+                    .padding()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                    Button(action: {
+                                            isFavorited.toggle()
+                                            // Aqui você pode adicionar a lógica para adicionar/remover a receita dos favoritos
+                                            if isFavorited {
+                                                // Adicione a receita aos favoritos
+                                            } else {
+                                                // Remova a receita dos favoritos
+                                            }
+                                        }) {
+                                            Image(systemName: isFavorited ? "heart.fill" : "heart")
+                                                .resizable()
+                                                .foregroundColor(isFavorited ? .red : .gray)
+                                                .frame(width: 35, height: 30, alignment: .center)
+                                        }
+                    Spacer()
+                }
             }
         }
     }
@@ -44,6 +66,6 @@ struct RecipeTypeView: View {
 
 struct RecipeTypeView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeTypeView(title: "Massas", image: "TESTE")
+        RecipeTypeView(title: "Massas", image: "https://static.wixstatic.com/media/4798a8_0ff820b2a2ed4f25b99dd853ac8ace77~mv2.jpg/v1/fill/w_640,h_428,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/4798a8_0ff820b2a2ed4f25b99dd853ac8ace77~mv2.jpg")
     }
 }
