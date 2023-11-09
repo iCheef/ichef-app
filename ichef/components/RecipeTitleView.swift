74a0e900582135f582469fbc9a1a61c7c272a0d6
João Goulart <joao123goulart@gmail.com>
2023-11-08 21:12:55 -0300

    Home view sem receita

--- ichef/components/RecipeTitleView.swift
+++ ichef/components/RecipeTitleView.swift
@@ -8,18 +8,18 @@
 import SwiftUI
 
 struct RecipeTitleView: View {
-    @State public var title     : String
-    @State public var time      : Int
-    @State public var portions  : Int
+    @State public var title      : String
+    @State public var time       : String
+    @State public var portions   : String
     @State public var difficulty : String
     
     var body: some View {
-        ZStack{
+        ZStack {
             RoundedRectangle(cornerRadius: 12)
                 .frame(height: 75)
                 .foregroundColor(.black.opacity(0.39))
             
-            VStack{
+            VStack {
                 Text(title)
                     .font(.title)
                     .bold()
@@ -30,28 +30,27 @@ struct RecipeTitleView: View {
                         .font(.title2)
                         .foregroundColor(Color("Laranja"))
                         .padding(.horizontal)
+                    
                     Spacer()
-                    Text("\(portions) Porções")
+                    
+                    Text(time != "" ? "\(time) Minutos" : "")
                         .font(.title2)
                         .foregroundColor(Color("Laranja"))
+                    
                     Spacer()
-                    Text("30 Porções")
+                    
+                    Text(portions != "" ? "\(portions) Porções" : "")
                         .font(.title2)
                         .foregroundColor(Color("Laranja"))
                         .padding(.horizontal)
                 }
-                
-                
             }
-            
-            
         }
-        
     }
 }
 
 struct RecipeTitleView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeTitleView(title: "Macarrão ao molho branco", time: 30, portions: 10, difficulty: "Fácil")
+        RecipeTitleView(title: "Macarrão ao molho branco", time: "10", portions: "30", difficulty: "facil")
     }
 }
